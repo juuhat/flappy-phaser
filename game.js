d331bba944e5cd780d580d108912f3712248290d
Juuso Hatakka <rasek24@gmail.com>
2015-03-23 00:38:50 +0200

    added scoring

--- game.js
+++ game.js
@@ -5,6 +5,10 @@ var gravity = 900;
 var flapPower = 300;
 var pipeSpeed = 50;
 var pipes;
+var holeSize = 120;
+
+var score;
+var scoreText;
 
 function preload() {
 	game.load.image("player", "assets/player.png");
@@ -14,6 +18,10 @@ function preload() {
 function create() {
 	game.stage.backgroundColor = "#80CCCC";
 	game.physics.startSystem(Phaser.Physics.Arcade);
+
+	scoreText = game.add.text(10,10,"-",{
+		font:"bold 16px Arial"
+	});
 
 	player = game.add.sprite(80, 240, "player");
 	game.physics.arcade.enable(player);
@@ -29,6 +37,7 @@ function create() {
 
 	addPipe();
 
+	score = 0;
 }
 
 function update() {
@@ -37,21 +46,20 @@ function update() {
 	if (player.y < -50 || player.y > 460) {
 		die();
 	}
+
+	pipes.forEach(function(p) {
+		if (p.x <= player.x && !p.hasScored) {
+			p.hasScored = true;
+			score += 0.5;
+			scoreText.text = score;
+		}
+	});
+
 }
 
 function addPipe() {
-	var pipeHole = 100;
-	var pipeHolePosition = game.rnd.between(50, 430-pipeHole);
-
-	var upperPipe = game.add.sprite(320, pipeHolePosition-480, "pipe");
-	game.physics.arcade.enable(upperPipe, Phaser.Physics.Arcade);
-	upperPipe.body.velocity.x = -pipeSpeed;
-	pipes.add(upperPipe);
-
-	var lowerPipe = game.add.sprite(320, pipeHolePosition+pipeHole, "pipe");
-	game.physics.arcade.enable(lowerPipe, Phaser.Physics.Arcade);
-	lowerPipe.body.velocity.x = -pipeSpeed;
-	pipes.add(lowerPipe);
+	var pipeHolePosition = game.rnd.between(50, 430-holeSize);
+	var pipes = new PipeGroup(pipeHolePosition);
 }
 
 function flap() {
@@ -60,4 +68,18 @@ function flap() {
 
 function die() {
 	game.state.start(game.state.current);
+}
+
+var PipeGroup = function(pipeHolePosition) {
+	var upperPipe = game.add.sprite(320, pipeHolePosition-480, "pipe");
+	game.physics.arcade.enable(upperPipe, Phaser.Physics.Arcade);
+	upperPipe.body.velocity.x = -pipeSpeed;
+	pipes.add(upperPipe);
+
+	var lowerPipe = game.add.sprite(320, pipeHolePosition+holeSize, "pipe");
+	game.physics.arcade.enable(lowerPipe, Phaser.Physics.Arcade);
+	lowerPipe.body.velocity.x = -pipeSpeed;
+	pipes.add(lowerPipe);
+
+	this.hasScored = false;
 }
