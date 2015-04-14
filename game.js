03ffe6dde06246da130f8a2769c3f69f9899ba3c
Juuso Hatakka <rasek24@gmail.com>
2015-04-14 22:33:05 +0300

    refactored pipe group creation

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 var game = new Phaser.Game(320, 480, Phaser.CANVAS, null, {preload: preload, create: create, update: update});
 
 var player;
+var pipes;
 var gravity = 900;
 var flapPower = 300;
 var pipeSpeed = 50;
-var pipes;
 var holeSize = 120;
 
 var score;
@@ -19,7 +19,7 @@ function create() {
 	game.stage.backgroundColor = "#80CCCC";
 	game.physics.startSystem(Phaser.Physics.Arcade);
 
-	scoreText = game.add.text(10,10,"-",{
+	scoreText = game.add.text(10, 10 , "-", {
 		font:"bold 16px Arial"
 	});
 
@@ -48,18 +48,17 @@ function update() {
 	}
 
 	pipes.forEach(function(p) {
-		if (p.x <= player.x && !p.hasScored) {
+		if (p.x <= player.x && p.hasScored == false) {
 			p.hasScored = true;
-			score += 0.5;
+			score += 1;
 			scoreText.text = score;
 		}
 	});
-
 }
 
 function addPipe() {
-	var pipeHolePosition = game.rnd.between(50, 430-holeSize);
-	var pipes = new PipeGroup(pipeHolePosition);
+	var pipeHolePosition = game.rnd.between(50, 430 - holeSize);
+	createPipeGroup(pipeHolePosition);
 }
 
 function flap() {
@@ -70,16 +69,15 @@ function die() {
 	game.state.start(game.state.current);
 }
 
-var PipeGroup = function(pipeHolePosition) {
-	var upperPipe = game.add.sprite(320, pipeHolePosition-480, "pipe");
+function createPipeGroup(pipeHolePosition) {
+	var upperPipe = game.add.sprite(320, pipeHolePosition - 480, "pipe");
+	upperPipe.hasScored = false;
 	game.physics.arcade.enable(upperPipe, Phaser.Physics.Arcade);
 	upperPipe.body.velocity.x = -pipeSpeed;
 	pipes.add(upperPipe);
 
-	var lowerPipe = game.add.sprite(320, pipeHolePosition+holeSize, "pipe");
+	var lowerPipe = game.add.sprite(320, pipeHolePosition + holeSize, "pipe");
 	game.physics.arcade.enable(lowerPipe, Phaser.Physics.Arcade);
 	lowerPipe.body.velocity.x = -pipeSpeed;
 	pipes.add(lowerPipe);
-
-	this.hasScored = false;
 }
